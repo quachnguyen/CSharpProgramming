a478cb25bd36c27bbb339e97ed325ce888de7125
Quách Nguyên <quachngochoangnguyen@gmail.com>
2018-08-15 00:28:47 +0700

    Initial and complete code

---
+++ StackTrace/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Reflection;
+
+namespace StackTrace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Case1();
+            //Case2();
+            try
+            {
+                int threhold = 20;
+                int result = threhold / 0;
+            }
+            catch (Exception ex)
+            {
+                PreserveStackTrace(ex);
+                throw;
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Case1()
+        {
+            try
+            {
+                DeviviByZero();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        //Throw full exception 
+        private static void Case2()
+        {
+            try
+            {
+                DeviviByZero();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private static void DeviviByZero()
+        {
+            int threhold = 20;
+            int result = threhold / 0;
+        }
+
+        private static void PreserveStackTrace(Exception ex)
+        {
+            MethodInfo preserveStackTrace = typeof(Exception).GetMethod("InternalPreserveStackTrace", BindingFlags.Instance | BindingFlags.NonPublic);
+            preserveStackTrace.Invoke(ex, null);
+        }
+    }
+}
